2ce494b887d423900a90fcc5fd86b8b469da1ed9
Robin Wong <chaowang0313@gmail.com>
2024-06-01 03:58:59 +0800

    feat(web): use more headers

--- packages/web/server/trpc/routers/index.ts
+++ packages/web/server/trpc/routers/index.ts
@@ -26,9 +26,15 @@ export const appRouter = router({
         url: input.url,
         fetchOptions: {
           headers: {
-            // ios safari user agent
+            Accept:
+              "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh-Hans;q=0.9",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
             "User-Agent":
-              "Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1",
+              "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.5 Mobile/15E148 Safari/604.1",
           },
         },
       });
