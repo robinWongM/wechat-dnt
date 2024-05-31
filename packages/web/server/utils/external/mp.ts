7c4e18780d5ee3407bc530f866f90c7f88d29dc5
Robin Wong <chaowang0313@gmail.com>
2024-05-31 22:45:10 +0800

    feat(web): add more log

--- packages/web/server/utils/external/mp.ts
+++ packages/web/server/utils/external/mp.ts
@@ -62,17 +62,22 @@ export const mpSendTextMessage = async (toUser: string, content: string) => {
     },
   };
 
-  return fetch(
-    `${MP_SEND_TEXT_ENDPOINT}?access_token=${accessToken}`,
-    {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  ).catch((err) => {
-    console.error(err);
-    throw err;
-  });
-}
+  return fetch(`${MP_SEND_TEXT_ENDPOINT}?access_token=${accessToken}`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((resp) => {
+      console.log("API response:", resp.status, resp.statusText);
+      return resp.json();
+    })
+    .then((resp) => {
+      console.log("API response body:", resp);
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
+};
