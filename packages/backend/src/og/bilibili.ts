871681df949cb96b15e101298807c488abbe190a
Robin Wong <chaowang0313@gmail.com>
2024-06-05 22:11:41 +0800

    feat(backend): rm

--- packages/backend/src/og/bilibili.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  host: ["www.bilibili.com", "bilibili.com", "m.bilibili.com", "t.bilibili.com"],
-  title: (title: string) => title.replace(/_哔哩哔哩_bilibili/g, ""),
-  description: (description: string) => `📺 分享自 哔哩哔哩\n📖 ${description}`,
-}
--- packages/backend/src/og/xiaohongshu.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  host: ['www.xiaohongshu.com', 'xiaohongshu.com'],
-  description: (description: string) => `📕 分享自 小红书\n📖 ${description}`,
-}
