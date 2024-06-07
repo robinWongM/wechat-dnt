58392d240ecd5eb6188784f4298877f02091ce76
Robin Wong <chaowang0313@gmail.com>
2024-06-07 08:50:29 +0800

    feat(web): clean clipboard

--- packages/web/components/share/preview.vue
+++ packages/web/components/share/preview.vue
@@ -9,7 +9,8 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from '@/components/ui/drawer'
-import { useEventListener, useMutationObserver } from '@vueuse/core';
+import { Button } from '@/components/ui/button';
+import { useEventListener, useClipboard } from '@vueuse/core';
 
 const log = ref(0);
 const iframe = ref<HTMLIFrameElement | null>(null);
@@ -38,13 +39,34 @@ watchEffect(() => {
     isContentLoaded.value = false;
   }
 });
+
+const { copy } = useClipboard();
+const { $toast } = useNuxtApp();
+const clearClipboard = () => {
+  copy('')
+    .then(() => {
+      $toast.success('剪切板已清空。', {
+        duration: 2000,
+      });
+    }).catch(() => {
+      $toast.error('清空失败。可尝试手动清空。', {
+        duration: 2000,
+      });
+    });
+}
 </script>
 
 <template>
   <Drawer :open="modelValue" @update:open="onModelValueChange">
     <DrawerContent class="w-full drawer-content">
+      <DrawerHeader>
+        <div class="flex flex-row gap-4 items-center">
+          <div class="text-xs flex-1 text-left">部分网站在用户点击时会静默复制内容至剪切板，并在你下次打开 App 读取。为了防止被跟踪，你可以使用右边的按钮清空剪贴板。</div>
+          <Button class="flex-none" variant="secondary" size="sm" @click="clearClipboard">清空剪切板</Button>
+        </div>
+      </DrawerHeader>
       <iframe ref="iframe" :src="url" sandbox="allow-same-origin allow-scripts" allowfullscreen="true"
-        webkitallowfullscreen="true" credentialless class="mt-4 w-full h-full transition-opacity"
+        webkitallowfullscreen="true" credentialless class="focus:outline-none w-full h-full transition-opacity"
         referrerpolicy="no-referrer" @load="onLoad" :class="isContentLoaded ? 'opacity-100' : 'opacity-0'" />
     </DrawerContent>
   </Drawer>
